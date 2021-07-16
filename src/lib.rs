75aefc53df5fbcd13806068ec83131b42defa31e
Dania99dev <dania99.dev@gmail.com>
2021-07-16 23:43:30 +0430

    improvements based on chapter 13 (using iterators)

--- src/lib.rs
+++ src/lib.rs
@@ -8,13 +8,17 @@ pub struct Config {
   pub case_sensitive: bool,
 }
 impl Config {
-  pub fn new(args: &[String]) -> Result<Config, &str> {
-    if args.len() < 3 {
-      return Err("not enough arguments");
-    }
+  pub fn new(mut args: env::Args) -> Result<Config, &'static str> {
+    args.next();
 
-    let query = args[1].clone();
-    let filename = args[2].clone();
+    let query = match args.next() {
+      Some(query) => query,
+      None => return Err("Didn't get a query string"),
+    };
+    let filename = match args.next() {
+      Some(filename) => filename,
+      None => return Err("Didn't get a filename"),
+    };
 
     let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
 
@@ -41,13 +45,10 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-  let mut results = Vec::new();
-  for line in contents.lines() {
-    if line.contains(query) {
-      results.push(line);
-    }
-  }
-  results
+  contents
+    .lines()
+    .filter(|line| line.contains(query))
+    .collect()
 }
 
 fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
