8367537363f001d9658c6bf218c9d00cb894f6c4
Dania99dev <dania99.dev@gmail.com>
2021-07-16 23:46:42 +0430

    Iterators implemented on case_insensitive search

--- src/lib.rs
+++ src/lib.rs
@@ -54,13 +54,10 @@ fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
 fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
   let query = query.to_lowercase();
 
-  let mut results = Vec::new();
-  for line in contents.lines() {
-    if line.to_lowercase().contains(&query) {
-      results.push(line);
-    }
-  }
-  results
+  contents
+    .lines()
+    .filter(|line| line.to_lowercase().contains(&query))
+    .collect()
 }
 
 #[cfg(test)]
